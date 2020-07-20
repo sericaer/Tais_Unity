fb5b1bf5eeec342c6c66feda383323fe6402318f
sericaer <sericaer@outlook.com>
2020-07-20 17:36:56 +0800

    Add end Scene

---
+++ Assets/Scripts/MonoBehaviour/EndScene.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+using TaisEngine.Run;
+
+public class EndScene : MonoBehaviour
+{
+    Text histroyRecord;
+
+    // Use this for initialization
+    void Start()
+    {
+        histroyRecord.text = string.Join("\n", RunData.inst.recordMsg);
+        RunData.inst = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void onConfirm()
+    {
+        SceneManager.LoadScene("StarScene");
+    }
+}
