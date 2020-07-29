f42e13b141e77a1068b26b6aded641a0547bfb27
sericaer <sericaer@outlook.com>
2020-07-29 22:48:21 +0800

    修改cs

--- Assets/Scripts/MonoBehaviour/MainScene/top/EconomyDetail.cs
+++ Assets/Scripts/MonoBehaviour/MainScene/top/EconomyDetail.cs
@@ -8,121 +8,121 @@ using TaisEngine.Run;
 
 public class EconomyDetail : MonoBehaviour
 {
-    public Slider LocalCurrTaxSlider;
-    public Text LocalCurrTaxText;
+    //public Slider LocalCurrTaxSlider;
+    //public Text LocalCurrTaxText;
 
-    public Slider ChaotingExpectTaxSlider;
-    public Text ChaotingExpectTaxText;
+    //public Slider ChaotingExpectTaxSlider;
+    //public Text ChaotingExpectTaxText;
 
-    public Text surplusText;
-    public Text consumeText;
+    //public Text surplusText;
+    //public Text consumeText;
 
-    public Button btnConfirm;
+    //public Button btnConfirm;
 
-    public GameObject prefabConfirmDialog;
+    //public GameObject prefabConfirmDialog;
 
-    TaisEngine.Run.Economy economy;
-    TaisEngine.Run.Chaoting chaoting;
+    //TaisEngine.Run.Economy economy;
+    //TaisEngine.Run.Chaoting chaoting;
 
-    public void LocalCurrTaxSliderValueChanged(float value)
-    {
+    //public void LocalCurrTaxSliderValueChanged(float value)
+    //{
 
-        changedCurrTax = value - (float)economy.curr_tax_level;
-    }
+    //    changedCurrTax = value - (float)economy.curr_tax_level;
+    //}
 
-    public void OnConfirm()
-    {
-        var confirmDialog = Instantiate(prefabConfirmDialog, this.transform) as GameObject;
+    //public void OnConfirm()
+    //{
+    //    var confirmDialog = Instantiate(prefabConfirmDialog, this.transform) as GameObject;
 
-        var dialog = confirmDialog.GetComponent<DialogNextChangedConfirm>();
+    //    var dialog = confirmDialog.GetComponent<DialogNextChangedConfirm>();
 
-        dialog.desc = LocalString.Get("NEXT_CHANGED_DESC", 
-                                      CommonDef.TaxLevel.Get().tax_change_intervl.Value, 
-                                      GMDate.Parse(RunData.inst.date.total_days + CommonDef.TaxLevel.Get().tax_change_intervl.Value).ToString());
-        dialog.act = () =>
-        {
-            if (!changedCurrTax.Equals(0.0f))
-            {
-                economy.currTaxChanged(changedCurrTax);
-            }
+    //    dialog.desc = LocalString.Get("NEXT_CHANGED_DESC", 
+    //                                  CommonDef.TaxLevel.Get().tax_change_intervl.Value, 
+    //                                  GMDate.Parse(RunData.inst.date.total_days + CommonDef.TaxLevel.Get().tax_change_intervl.Value).ToString());
+    //    dialog.act = () =>
+    //    {
+    //        if (!changedCurrTax.Equals(0.0f))
+    //        {
+    //            economy.currTaxChanged(changedCurrTax);
+    //        }
 
-            this.gameObject.SetActive(false);
-        }; 
-    }
+    //        this.gameObject.SetActive(false);
+    //    }; 
+    //}
 
     public void OnCancel()
     {
-        this.gameObject.SetActive(false);
+        Destroy(this.gameObject);
     }
 
-    private float changedCurrTax;
+    //private float changedCurrTax;
 
-    private void OnEnable()
-    {
-        Timer.Pause();
+    //private void OnEnable()
+    //{
+    //    Timer.Pause();
 
-        RefreshData();
-    }
+    //    RefreshData();
+    //}
 
-    private void OnDisable()
-    {
-        changedCurrTax = 0;
-        Timer.unPause();
-    }
+    //private void OnDisable()
+    //{
+    //    changedCurrTax = 0;
+    //    Timer.unPause();
+    //}
 
-    private void Awake()
-    {
+    //private void Awake()
+    //{
         
-        economy = TaisEngine.Run.RunData.inst.economy;
-        chaoting = TaisEngine.Run.RunData.inst.chaoting;
+    //    economy = TaisEngine.Run.RunData.inst.economy;
+    //    chaoting = TaisEngine.Run.RunData.inst.chaoting;
 
-        LocalCurrTaxSlider.minValue = 0;
-        LocalCurrTaxSlider.maxValue = (float)TaisEngine.Run.Economy.TAX_LEVEL.levelmax;
+    //    LocalCurrTaxSlider.minValue = 0;
+    //    LocalCurrTaxSlider.maxValue = (float)TaisEngine.Run.Economy.TAX_LEVEL.levelmax;
 
-        ChaotingExpectTaxSlider.minValue = 0;
-        ChaotingExpectTaxSlider.maxValue = (float)TaisEngine.Run.Economy.TAX_LEVEL.levelmax;
+    //    ChaotingExpectTaxSlider.minValue = 0;
+    //    ChaotingExpectTaxSlider.maxValue = (float)TaisEngine.Run.Economy.TAX_LEVEL.levelmax;
 
-        changedCurrTax = 0;
+    //    changedCurrTax = 0;
 
-        ChaotingExpectTaxSlider.interactable = false;
-    }
+    //    ChaotingExpectTaxSlider.interactable = false;
+    //}
 
-    private void RefreshData()
-    {
-        ChaotingExpectTaxSlider.value = (float)chaoting.tax_level;
-        LocalCurrTaxSlider.value = (float)economy.curr_tax_level;
+    //private void RefreshData()
+    //{
+    //    ChaotingExpectTaxSlider.value = (float)chaoting.tax_level;
+    //    LocalCurrTaxSlider.value = (float)economy.curr_tax_level;
 
-        ChaotingExpectTaxText.text =  chaoting.expect_tax.ToString();
-        LocalCurrTaxText.text = economy.currTax.ToString();
+    //    ChaotingExpectTaxText.text =  chaoting.expect_tax.ToString();
+    //    LocalCurrTaxText.text = economy.currTax.ToString();
 
-        LocalCurrTaxSlider.interactable = economy.local_tax_change_valid;
-        //if(!LocalCurrTaxSlider.interactable)
-        //{
-        //    LocalCurrTaxSlider.GetComponent<TooltipTrigger>().funcGetTooltipStr = () =>
-        //    {
-        //        return ("", TaisEngine.Mod.GetLocalString("VALID_DAYS_SPAN", economy.taxChangedDaysSpan, TaisEngine.GMDate.ToString(economy.validTaxChangedDays)));
-        //    };
-        //}
-        //else
-        //{
-        //    LocalCurrTaxSlider.GetComponent<TooltipTrigger>().funcGetTooltipStr = () =>
-        //    {
-        //        return ("", "");
-        //    };
-        //}
-    }
+    //    LocalCurrTaxSlider.interactable = economy.local_tax_change_valid;
+    //    //if(!LocalCurrTaxSlider.interactable)
+    //    //{
+    //    //    LocalCurrTaxSlider.GetComponent<TooltipTrigger>().funcGetTooltipStr = () =>
+    //    //    {
+    //    //        return ("", TaisEngine.Mod.GetLocalString("VALID_DAYS_SPAN", economy.taxChangedDaysSpan, TaisEngine.GMDate.ToString(economy.validTaxChangedDays)));
+    //    //    };
+    //    //}
+    //    //else
+    //    //{
+    //    //    LocalCurrTaxSlider.GetComponent<TooltipTrigger>().funcGetTooltipStr = () =>
+    //    //    {
+    //    //        return ("", "");
+    //    //    };
+    //    //}
+    //}
 
-    private void Update()
-    {
-        LocalCurrTaxText.text = economy.getExpectTaxValue(LocalCurrTaxSlider.value).ToString();
+    //private void Update()
+    //{
+    //    LocalCurrTaxText.text = economy.getExpectTaxValue(LocalCurrTaxSlider.value).ToString();
 
-        var surplus = double.Parse(LocalCurrTaxText.text) - double.Parse(ChaotingExpectTaxText.text);
-        surplusText.text = surplus.ToString();
+    //    var surplus = double.Parse(LocalCurrTaxText.text) - double.Parse(ChaotingExpectTaxText.text);
+    //    surplusText.text = surplus.ToString();
 
-        consumeText.text = CommonDef.TaxLevel.getConsume(LocalCurrTaxSlider.value).ToString();
+    //    consumeText.text = CommonDef.TaxLevel.getConsume(LocalCurrTaxSlider.value).ToString();
 
 
-        btnConfirm.interactable = !changedCurrTax.Equals(0.0f);
-    }
+    //    btnConfirm.interactable = !changedCurrTax.Equals(0.0f);
+    //}
 
 }
